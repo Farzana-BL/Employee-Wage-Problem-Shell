a89829f6917afbfedf1e5d37fe38722cbf182df0
Farzana-BL <farzana.shaikh@bridgelabz.com>
2023-05-25 12:33:39 +0530

    [Farzana] Add : This script is for calculating the daily wage of an Employee

--- emp.sh
+++ emp.sh
@@ -4,10 +4,15 @@ echo "Welcome To Employee Wage Computation Program"
 
 
 chk_attendence=$((RANDOM%2))
+wage_per_hour=20
+full_day_hours=8
 
 if [ $chk_attendence -eq 1 ]
 then 
     echo "Employee is Present"
+    daily_wage=$(($wage_per_hour*$full_day_hours))
+    echo "Daily Wage of an employee is $daily_wage"
+
 else    
      echo "Employee is Absent"
 fi
